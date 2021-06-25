83080f6f168ad368e8dd772ff60755be06864b44
Hajcik <hajcik666pl@gmail.com>
2021-06-25 17:37:29 +0200

    Update script.js

--- LIVE/script.js
+++ LIVE/script.js
@@ -29,6 +29,20 @@ function main() {
     Table();
 }
 
+/* TABLE
+Jak sprawdzać kolor kratki? Pobierać z canvas kolor piksela?
+Tak też można, ale jest to nieefektywne. Na pewno lepsze będą
+tablice, w których zapamiętamy stan kratek, i które za chwilę
+będą niezbędne w celu uruchomienia „życia”.
+Deklarujemy dwie tablice globalne (bez var) i je zerujemy:
+SZAtab – przechowujemy stan kratek: 0-szara, 1-biała
+SZAsas – liczba białych sąsiadów wokół kratki
+Tablice mają z „każdej strony” dodatkowy wiersz i kolumnę o
+numerach 0 i 101 – nie są używane, ale będą bardzo użyteczne
+podczas wyszukiwania i obliczania – nie musimy stosować dodatkowych warunków brzegowych.
+Funkcję TABLICE wstawiamy do funkcji START
+*/
+
 function Table() {
     chessboard_table = new Array(101);
     chessboard_neighbor_number = new Array(101);
@@ -41,6 +55,8 @@ function Table() {
         }
     }
 }
+
+
 
 function FStartStop() {
     change_color = !change_color;
@@ -58,6 +74,16 @@ canvas_object.canvas.onclick = function (evt) {
     chessboard_table[SQUARE_POSITION.x][SQUARE_POSITION.y] = !chessboard_table[SQUARE_POSITION.x][SQUARE_POSITION.y];
 };
 
+/*
+SUWAK
+Szachownica powinna
+zmieniać swoje wymiary i
+automatycznie dopasowywać
+się do wielkości okna w
+przeglądarce – zrealizujemy to za pomocą suwaka.
+Zakładamy, że szachownica może mieć od 1 do 100 kratek.
+Odpowiednie instrukcje w HTML i funkcja FSuwak je obsługująca
+*/
 function ZIP_change() {
     square_number = zip.value;
     zip_TXT.value = square_number;
@@ -81,6 +107,21 @@ function Chess(x, y, side, number, color, color_in) {
     }
 }
 
+/* PAINT_CHESS
+Gdy klikamy myszą w kratkę powinna zapalić się na biało, gdy klikniemy powtórnie - ma wrócić do stanu
+początkowego (kolor szary). Aby było to możliwe musimy „zapalać” – wstawiać 1 lub „gasić” – wstawiać
+0, do odpowiednich komórek tablicy SZAtab.
+Zamiana zawartość klikniętej
+komórki tablicy na przeciwną.
+Nie jest nam już potrzebne zapalanie i gaszenie w funkcji
+onclick – usuwamy wyliczanie współrzędnych x1 i y1
+oraz rysowanie kwadratu – szachownica będzie rysowana
+na podstawie tablicy.
+Nowa szachownica będzie rysowana na podstawie
+zawartości tablicy SZAtab
+Funkcję RYSUJ_SZACHY wstawiamy do pętli
+animacyjnej zamiast funkcji SZACHY
+*/
 function Paint_chess(side, number) {
     for (var canvas_width = 1; canvas_width <= number; canvas_width++) {
         for (var square = 1; square <= number; square++) {
@@ -95,7 +136,18 @@ function Paint_chess(side, number) {
         }
     }
 }
-
+/*
+Umiemy zmieniać szachownicę, a także ustawiać początkowe położenie robaczków. Aby „puścić w ruch”
+naszą grę należy wykonać jeszcze dwie czynności:
+- wyliczyć ile robaczków znajduje się wokół każdego pola
+- usunąć lub wstawić robaczki na podstawie ilości sąsiadów
+Funkcja WYLICZ_SZACHY „przelatuje” po kolei przez wszystkie
+kratki szachownicy i w każdej z nich „rozgląda” się wokół (8 pól) i
+sumuje ile jest robaczków (zapalonych pól).
+Zaglądamy do tablicy SZAtab
+Zliczamy robaczki w zmiennej SUMA
+Wynik zapisujemy do tablicy SZAsas
+*/
 function Count_chess() {
     for (var canvas_width = 1; canvas_width < square_number; canvas_width++) {
         for (var square = 1; square < square_number; square++) {
@@ -112,7 +164,16 @@ function Count_chess() {
         }
     }
 }
-
+/*
+Skoro mamy obliczoną ilość robaczków, to możemy „wyliczyć”, jakie będzie położenie nowego pokolenia
+robaczków.
+Nowy robacze rodzi się gdy ma
+dokładnie trzech sąsiadów
+Robaczek umiera, gdy ma mniej
+niż dwóch sąsiadów
+Robaczek umiera, gdy ma więcej
+niż 3 sąsiadów
+*/
 function Count_generation() {
     for (var canvas_width = 1; canvas_width < square_number; canvas_width++) {
         for (var square = 1; square < square_number; square++) {
